4174274c17db8c9a4d816da31e507e574e002cf9
Nicolás Acosta <nicolasacostap@gmail.com>
2025-05-14 10:58:58 +0200

    Update BikeConfigurator.tsx

--- src/components/bike-configurator/BikeConfigurator.tsx
+++ src/components/bike-configurator/BikeConfigurator.tsx
@@ -1,4 +1,28 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 export default function BikeConfigurator() {
 
-  return (<div>BikeConfigurator</div>);
+
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Customize your bike 🚲</CardTitle>
+        <CardDescription>Select each part of your dream bike! Don't worry, all the prices will be calculated on the fly.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div>I'm the configurator!</div>
+      </CardContent>
+      <CardFooter className="flex justify-end">
+        <Button className="cursor-pointer">Order my bike 🚀</Button>
+      </CardFooter>
+    </Card>
+  );
 }
