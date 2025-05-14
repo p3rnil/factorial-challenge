2844d9905c03417b0ce5a60917d6d1dfdc71fa4f
Nicolás Acosta <nicolasacostap@gmail.com>
2025-05-14 09:32:54 +0200

    Add Ticket

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import BikeConfigurator from '@/components/bike-configurator/BikeConfigurator'
+import Ticket from '@/components/Ticket'
 
 function App() {
   return (
     <>
       <div>Marku's bike shop</div>
       <BikeConfigurator />
+      <Ticket />
     </>
   )
 }
